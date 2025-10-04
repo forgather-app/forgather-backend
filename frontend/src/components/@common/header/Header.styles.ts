567f96f7d233f9df8cdb67e378432cc6b8fa79fb
hanheel <hanhee630630@gmail.com>
2025-10-04 17:51:21 +0900

    refactor: 헤더 스타일 변경

--- frontend/src/components/@common/header/Header.styles.ts
+++ frontend/src/components/@common/header/Header.styles.ts
@@ -5,8 +5,10 @@ export const HeaderContainer = styled.header<{ $mode: HeaderMode }>`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  max-width: ${({ theme }) => theme.layout.width};
   width: 100%;
   height: 60px;
+  margin: 0 auto;
   padding: 0 ${({ theme }) => theme.layout.padding.leftRight};
   background-color: ${({ theme, $mode }) =>
     $mode === 'dark' ? theme.colors.gray06 : theme.colors.background};
