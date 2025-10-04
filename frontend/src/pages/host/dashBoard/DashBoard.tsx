009de63490a2211c30a451381e7bd71bef937d1b
hanheel <hanhee630630@gmail.com>
2025-10-04 21:40:40 +0900

    feat: DashBoard 페이지 제작

---
+++ frontend/src/pages/host/dashBoard/DashBoard.tsx
@@ -0,0 +1,33 @@
+import Button from '../../../components/@common/buttons/button/Button';
+import InfoRow from '../../../components/host/infoRow/InfoRow';
+import * as C from '../../../styles/@common/Typography.styles';
+import { mockDashboardData } from '../../mockData';
+import { Thumbnail } from '../Host.common.styles';
+import * as S from './DashBoard.styles';
+
+const DashBoard = () => {
+  return (
+    <S.Wrapper>
+      <C.Title>스페이스 대시보드</C.Title>
+      <Thumbnail src={mockDashboardData.thumbnail} />
+      <S.InfoRowContainer>
+        <InfoRow label="스페이스 이름" value={mockDashboardData.title} />
+        <InfoRow
+          label="방명록 공개 범위"
+          value={mockDashboardData.publicRange}
+        />
+        <InfoRow label="스페이스 설명" value={mockDashboardData.introduction} />
+        <InfoRow label="E-mail" value={mockDashboardData.email} />
+        <InfoRow label="Instagram" value={mockDashboardData.instagramId} />
+      </S.InfoRowContainer>
+
+      <S.DeleteButtonContainer>
+        <Button variant="error" text="스페이스 삭제" onClick={() => {}} />
+      </S.DeleteButtonContainer>
+
+      <Button variant="primary" text="수정하기" onClick={() => {}} />
+    </S.Wrapper>
+  );
+};
+
+export default DashBoard;
