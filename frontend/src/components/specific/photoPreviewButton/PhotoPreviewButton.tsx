1ee39e4be80d1693113e03875834336e9f53fd89
hanheel <hanhee630630@gmail.com>
2025-10-10 00:26:28 +0900

    fix: 사진 삭제시 같은 사진일 경우 재업로드가 안되는 문제 해결

--- frontend/src/components/specific/photoPreviewButton/PhotoPreviewButton.tsx
+++ frontend/src/components/specific/photoPreviewButton/PhotoPreviewButton.tsx
@@ -1,56 +1,83 @@
-import { useId } from 'react';
-import { IoCamera } from 'react-icons/io5';
+import { useId, useRef } from 'react';
+import { IoCamera, IoClose } from 'react-icons/io5';
 import defaultImage from '../../../@assets/images/default-image.png';
 import { Thumbnail } from '../../../pages/MainPage.common.styles';
 import type { PreviewFile } from '../../../types/file.type';
 import { createImageErrorHandler } from '../../../utils/createImageErrorHandler';
-import Button from '../../@common/buttons/button/Button';
+import { handleKeyboardClick } from '../../../utils/keyboard';
 import * as S from './PhotoPreviewButton.styles';
 
 interface PhotoPreviewButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   uploadImage: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  deleteImage: () => void;
+  clearFiles: () => void;
   previewFile: PreviewFile[];
   originalSrc?: string;
 }
 
 const PhotoPreviewButton = ({
   uploadImage,
+  deleteImage,
   originalSrc,
   previewFile,
+  clearFiles,
 }: PhotoPreviewButtonProps) => {
   const fileInputId = useId();
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const matchThumbnailImage = () =>
     previewFile[0]?.previewUrl || originalSrc || defaultImage;
 
   const isPhotoExist = !!previewFile[0]?.previewUrl && !!originalSrc;
 
+  const deletePhoto = () => {
+    if (originalSrc) {
+      deleteImage();
+    }
+    // TODO : 성공시 아래 로직 실행
+    clearFiles();
+  };
+
   return (
     <S.Wrapper>
-      <S.Label htmlFor={fileInputId}>
+      {isPhotoExist && (
+        <S.OverlayButton
+          $position="top"
+          role="button"
+          tabIndex={0}
+          aria-label="사진 삭제"
+          onKeyDown={handleKeyboardClick}
+          onClick={deletePhoto}
+        >
+          <IoClose />
+        </S.OverlayButton>
+      )}
+      <S.Label
+        htmlFor={fileInputId}
+        tabIndex={0}
+        onKeyDown={handleKeyboardClick}
+      >
+        <S.Overlay $isPhotoExist={isPhotoExist}>
+          <IoCamera />
+        </S.Overlay>
         <Thumbnail
           src={matchThumbnailImage()}
           onError={createImageErrorHandler(defaultImage)}
         />
-        <S.Overlay isPhotoExist={isPhotoExist}>
-          <IoCamera />
-        </S.Overlay>
       </S.Label>
       <S.FileInput
         id={fileInputId}
         type="file"
         accept="image/*"
-        onChange={uploadImage}
+        onChange={(e) => {
+          uploadImage(e);
+          if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+          }
+        }}
+        ref={fileInputRef}
       />
-      {isPhotoExist && (
-        <Button
-          type="button"
-          variant="tertiary"
-          text="사진 삭제"
-          onClick={() => {}}
-        />
-      )}
     </S.Wrapper>
   );
 };
