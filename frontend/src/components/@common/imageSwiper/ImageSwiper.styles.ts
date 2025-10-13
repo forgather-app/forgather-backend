a1c108f7ab792714c737283bcd28dfe8fa0e0915
hanheel <hanhee630630@gmail.com>
2025-10-13 17:19:11 +0900

    refactor: 이미지가 없을 경우 대응

--- frontend/src/components/@common/imageSwiper/ImageSwiper.styles.ts
+++ frontend/src/components/@common/imageSwiper/ImageSwiper.styles.ts
@@ -1,33 +1,52 @@
 import styled from '@emotion/styled';
 
-export const ImageSwiperContainer = styled.div`
+export const NoImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
+export const NoImageComment = styled.h2`
+  ${({ theme }) => ({ ...theme.typography.bodyLarge })}
+  color: ${({ theme }) => theme.colors.gray03};
+  text-align: center;
+`;
+
+export const ImageSwiperContainer = styled.div`
   & .swiper {
     max-width: ${({ theme }) => theme.layout.width};
     max-height: 300px;
-    padding-bottom: 40px;
+    height: fit-content;
+    padding-bottom: 30px;
     display: flex;
     justify-content: center;
     align-items: center;
+    position: relative;
   }
   & .swiper-slide {
     width: 45%;
+    height: 100%;
     display: flex;
     justify-content: center;
-    align-items: center; 
-    transition: opacity 0.8s ease-in-out
+    align-items: center;
+    transition: opacity 0.8s ease-in-out;
+    border-radius: 4px;
   }
-  & .swiper-slide-prev{
+  & .swiper-slide-prev img {
     opacity: 0.8;
   }
-  & .swiper-slide-next{
+  & .swiper-slide-next img {
     opacity: 0.8;
   }
   & .swiper-pagination {
     ${({ theme }) => ({ ...theme.typography.captionSmall })}
-    color: ${({ theme }) => theme.colors.gray03};
+    color: ${({ theme }) => theme.colors.gray04};
+    position: absolute;
+    bottom: 0;
   }
-  & img{
+  & img {
     width: 100%;
     height: 100%;
     border-radius: 4px;
