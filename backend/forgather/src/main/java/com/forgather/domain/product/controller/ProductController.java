cdb1f63fbcf746766fad6465fc3053ae2ac2687b
kjyyjk <rlawnsdud920@gmail.com>
2025-11-20 18:10:23 +0900

    refactor: api uri path가 변경된 api는 버전 1로 초기화

--- backend/forgather/src/main/java/com/forgather/domain/product/controller/ProductController.java
+++ backend/forgather/src/main/java/com/forgather/domain/product/controller/ProductController.java
@@ -93,7 +93,7 @@ public class ProductController {
      */
     @SecurityRequirement(name = "bearerAuth")
     @Operation(summary = "작품 수정",
-        description = "변경 사항이 없는 데이터는 json에 포함하지 않거나 null로 요청한다.  복수 작품 등록이 반영된 api는 version 3으로 호출")
+        description = "변경 사항이 없는 데이터는 json에 포함하지 않거나 null로 요청한다.  복수 작품 등록이 반영된 api는 version 1로 호출")
     @PatchMapping(headers = "X-API-Version=2")
     public ResponseEntity<ProductResponse> updateV2(
         @PathVariable(value = "spaceCode") String spaceCode,
@@ -106,7 +106,7 @@ public class ProductController {
 
     @SecurityRequirement(name = "bearerAuth")
     @Operation(summary = "작품 수정",
-        description = "변경 사항이 없는 데이터는 json에 포함하지 않거나 null로 요청한다.  복수 작품 등록이 반영된 api는 version 3으로 호출")
+        description = "변경 사항이 없는 데이터는 json에 포함하지 않거나 null로 요청한다.  복수 작품 등록이 반영된 api는 version 1로 호출")
     @PatchMapping(value = "/{productId}", headers = "X-API-Version=1")
     public ResponseEntity<ProductResponse> updateV3(
         @PathVariable(value = "spaceCode") String spaceCode,
@@ -133,7 +133,7 @@ public class ProductController {
     }
 
     @SecurityRequirement(name = "bearerAuth")
-    @Operation(summary = "작품 삭제", description = "복수 작품 등록이 반영된 api는 version 2로 호출")
+    @Operation(summary = "작품 삭제", description = "복수 작품 등록이 반영된 api는 version 1로 호출")
     @DeleteMapping(value = "/{productId}", headers = "X-API-Version=1")
     public ResponseEntity<Void> deleteV2(
         @PathVariable(value = "spaceCode") String spaceCode,
