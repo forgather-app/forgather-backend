853b35d72e2d4d9a90e05d0e955eba0992c18311
kjyyjk <rlawnsdud920@gmail.com>
2025-11-20 18:38:02 +0900

    refactor: ProductsResponse 생성자 방어적 복사

--- backend/forgather/src/main/java/com/forgather/domain/product/dto/ProductsResponse.java
+++ backend/forgather/src/main/java/com/forgather/domain/product/dto/ProductsResponse.java
@@ -1,5 +1,6 @@
 package com.forgather.domain.product.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record ProductsResponse(
@@ -7,6 +8,6 @@ public record ProductsResponse(
 ) {
 
     public ProductsResponse(List<SimpleProductResponse> products) {
-        this.products = products;
+        this.products = new ArrayList<>(products);
     }
 }
