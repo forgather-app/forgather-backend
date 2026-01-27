8bcbdf2669b59253d04f272f9ce73319f27d3a61
ykmxxi <ykmxxi97@gmail.com>
2026-01-27 15:28:02 +0900

    feat: 인메모리 세션 저장소 세션 조회/삭제 기능 구현

--- src/main/java/com/forgather/back_office/auth/session/InMemorySessionStore.java
+++ src/main/java/com/forgather/back_office/auth/session/InMemorySessionStore.java
@@ -6,14 +6,29 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Component;
 
 import com.forgather.back_office.model.AdminSession;
+import com.forgather.back_office.model.SessionId;
+import com.forgather.global.exception.NotFoundException;
 
 @Component
 public class InMemorySessionStore implements SessionStore {
 
-    private final Map<String, AdminSession> sessions = new ConcurrentHashMap<>();
+    private final Map<SessionId, AdminSession> sessions = new ConcurrentHashMap<>();
 
     @Override
     public void save(AdminSession session) {
-        sessions.put(session.getSessionIdValue(), session);
+        sessions.put(session.getSessionId(), session);
+    }
+
+    @Override
+    public AdminSession getBySessionId(SessionId sessionId) {
+        if (!sessions.containsKey(sessionId)) {
+            throw new NotFoundException("어드민 세션이 존재하지 않습니다.");
+        }
+        return sessions.get(sessionId);
+    }
+
+    @Override
+    public void delete(SessionId sessionId) {
+        sessions.remove(sessionId);
     }
 }
--- src/main/java/com/forgather/back_office/auth/session/SessionStore.java
+++ src/main/java/com/forgather/back_office/auth/session/SessionStore.java
@@ -1,8 +1,13 @@
 package com.forgather.back_office.auth.session;
 
 import com.forgather.back_office.model.AdminSession;
+import com.forgather.back_office.model.SessionId;
 
 public interface SessionStore {
 
     void save(AdminSession session);
+
+    AdminSession getBySessionId(SessionId sessionId);
+
+    void delete(SessionId sessionId);
 }
