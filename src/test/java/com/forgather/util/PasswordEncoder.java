d2abeb3eaa90390ad717504e7018ddb5101b00d4
ykmxxi <ykmxxi97@gmail.com>
2026-02-03 15:30:42 +0900

    feat: 임시 어드민 유저 패스워드 생성기

---
+++ src/test/java/com/forgather/util/PasswordEncoder.java
@@ -0,0 +1,20 @@
+package com.forgather.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * 어드민 유저 생성 시 암호화된 비밀번호를 얻기 위한 유틸리티.
+ * main 메서드를 실행하면 평문 비밀번호가 BCrypt로 암호화되어 출력된다.
+ */
+public class PasswordEncoder {
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        String rawPassword = "password";
+
+        String encodedPassword = encoder.encode(rawPassword);
+        System.out.println("평문: " + rawPassword);
+        System.out.println("암호화: " + encodedPassword);
+    }
+}
