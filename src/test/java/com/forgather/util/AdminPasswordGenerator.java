66c68dbd4eab151d66b1417d2ebc2bc6b37c2ec6
ykmxxi <ykmxxi97@gmail.com>
2026-02-03 23:02:52 +0900

    refactor: 빈 충돌 방지를 위한 네이밍 변경

--- src/test/java/com/forgather/util/AdminPasswordGenerator.java
+++ src/test/java/com/forgather/util/AdminPasswordGenerator.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * 어드민 유저 생성 시 암호화된 비밀번호를 얻기 위한 유틸리티.
  * main 메서드를 실행하면 평문 비밀번호가 BCrypt로 암호화되어 출력된다.
  */
-public class PasswordEncoder {
+public class AdminPasswordGenerator {
 
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
