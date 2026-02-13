897e3cb8bb9f471200af440731b800f87d2f0f9a
ykmxxi <ykmxxi97@gmail.com>
2026-02-13 14:07:44 +0900

    fix: 조회 쿼리에 누락된 소프트 딜리트 여부 조건절 추가

--- src/main/java/com/forgather/back_office/repository/AdminSpaceHostMapRepository.java
+++ src/main/java/com/forgather/back_office/repository/AdminSpaceHostMapRepository.java
@@ -13,9 +13,10 @@ public interface AdminSpaceHostMapRepository {
     @Query("""
         SELECT shm
         FROM SpaceHostMap shm
-        JOIN FETCH shm.space s
+            JOIN FETCH shm.space s
         WHERE shm.host = :host
-        AND s.deletedAt IS NULL
+            AND s.deletedAt IS NULL
+            AND shm.deletedAt IS NULL
         ORDER BY s.createdAt DESC
         """)
     List<SpaceHostMap> findAllByHostAndDeletedAtIsNullWithSpaceOrderByCreatedAtDesc(@Param("host") Host host);
