10e91ea6c717dddb548797e14a00ab0741012d0f
ykmxxi <ykmxxi97@gmail.com>
2026-02-13 14:07:11 +0900

    fix: 잘못된 엔티티 타입 수정

--- src/main/java/com/forgather/back_office/repository/jpa/AdminSpaceHostMapJpaRepository.java
+++ src/main/java/com/forgather/back_office/repository/jpa/AdminSpaceHostMapJpaRepository.java
@@ -2,7 +2,8 @@ package com.forgather.back_office.repository.jpa;
 
 import com.forgather.back_office.repository.AdminSpaceHostMapRepository;
 import com.forgather.back_office.repository.ReadOnlyRepository;
-import com.forgather.domain.space.model.Space;
+import com.forgather.global.auth.model.SpaceHostMap;
 
-public interface AdminSpaceHostMapJpaRepository extends ReadOnlyRepository<Space, Long>, AdminSpaceHostMapRepository {
+public interface AdminSpaceHostMapJpaRepository
+    extends ReadOnlyRepository<SpaceHostMap, Long>, AdminSpaceHostMapRepository {
 }
