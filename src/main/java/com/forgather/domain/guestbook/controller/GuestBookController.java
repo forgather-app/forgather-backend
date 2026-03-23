e8926fc7d991f4ef046a0f127342d2e7b1ec43b1
kjyyjk <rlawnsdud920@gmail.com>
2026-03-23 11:19:33 +0900

    refactor: 주석 제거

--- src/main/java/com/forgather/domain/guestbook/controller/GuestBookController.java
+++ src/main/java/com/forgather/domain/guestbook/controller/GuestBookController.java
@@ -63,8 +63,6 @@ public class GuestBookController {
                 example = "createdAt,desc",
                 array = @ArraySchema(schema = @Schema(type = "string"))
             )
-            // 향후 페이지네이션 API가 추가되어 동일한 파라미터 정의가 반복될 경우,
-            // @Parameters 메타 어노테이션을 활용한 @PageableParameters 커스텀 어노테이션으로 중복을 제거할 수 있다.
         }
     )
     @GetMapping
