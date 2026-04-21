700af222b06659358fc47d5399d8a7ac2cb52e99
ykmxxi <ykmxxi97@gmail.com>
2026-04-21 13:49:29 +0900

    feat: 전시 시간(ExhibitionTime) 엔티티 구현

---
+++ src/main/java/com/forgather/domain/exhibition/model/ExhibitionTime.java
@@ -0,0 +1,41 @@
+package com.forgather.domain.exhibition.model;
+
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ExhibitionTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exhibition_id", nullable = false)
+    private Exhibition exhibition;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "day_type", nullable = false)
+    private DayType dayType;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalTime endTime;
+}
