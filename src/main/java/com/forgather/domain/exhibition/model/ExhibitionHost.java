4425bde4782f8d09c977b071c463149291b54d59
ykmxxi <ykmxxi97@gmail.com>
2026-04-21 15:14:12 +0900

    feat: 전시 호스트(ExhibitionHost) 엔티티 구현

---
+++ src/main/java/com/forgather/domain/exhibition/model/ExhibitionHost.java
@@ -0,0 +1,36 @@
+package com.forgather.domain.exhibition.model;
+
+import com.forgather.global.auth.model.Host;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ExhibitionHost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exhibition_id", nullable = false)
+    private Exhibition exhibition;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "host_id", nullable = false)
+    private Host host;
+
+    @Column(name = "is_creator", nullable = false)
+    private boolean isCreator;
+}
