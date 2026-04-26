1d660188321f4ea6e679949dc76d1102abcbf163
ykmxxi <ykmxxi97@gmail.com>
2026-04-26 19:17:06 +0900

    feat: 공통 api 포맷 성공 케이스 구현
    - 공통 API 응답은 code, message, data로 이루어진다

---
+++ src/main/java/com/forgather/global/response/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.forgather.global.response;
+
+public record ApiResponse<T>(
+    ResponseCode code,
+    String message,
+    T data
+) {
+
+    public static ApiResponse<Void> success() {
+        return new ApiResponse<>(ResponseCode.SUCCESS, null, null);
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(ResponseCode.SUCCESS, null, data);
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(ResponseCode.SUCCESS, message, data);
+    }
+}
