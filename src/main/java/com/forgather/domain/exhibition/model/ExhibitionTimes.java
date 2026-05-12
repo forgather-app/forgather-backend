fe0e8329187883bf03bce4bc5a32a5ef63125550
ykmxxi <ykmxxi97@gmail.com>
2026-05-12 20:42:29 +0900

    refactor: 변수명 변경

--- src/main/java/com/forgather/domain/exhibition/model/ExhibitionTimes.java
+++ src/main/java/com/forgather/domain/exhibition/model/ExhibitionTimes.java
@@ -26,9 +26,9 @@ public class ExhibitionTimes {
     }
 
     private void validateDayOfWeekDuplicate(List<ExhibitionTime> exhibitionTimes) {
-        Set<DayOfWeek> seen = EnumSet.noneOf(DayOfWeek.class);
+        Set<DayOfWeek> dayOfWeeks = EnumSet.noneOf(DayOfWeek.class);
         for (ExhibitionTime exhibitionTime : exhibitionTimes) {
-            if (!seen.add(exhibitionTime.getDayOfWeek())) {
+            if (!dayOfWeeks.add(exhibitionTime.getDayOfWeek())) {
                 throw new BaseException("전시 운영 시간에 같은 요일이 중복되었습니다.");
             }
         }
