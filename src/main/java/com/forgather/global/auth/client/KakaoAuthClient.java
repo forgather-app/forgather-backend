4539edb0923d244ddca6431d3c4796a9704ed178
kjyyjk <rlawnsdud920@gmail.com>
2026-07-02 16:53:40 +0900

    refactor: KakaoAuthClient deprecated

--- src/main/java/com/forgather/global/auth/client/KakaoAuthClient.java
+++ src/main/java/com/forgather/global/auth/client/KakaoAuthClient.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
  * 카카오 인증 관련 클라이언트
  * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-token
  */
+@Deprecated(forRemoval = true)
+@SuppressWarnings("removal")
 @Component
 @RequiredArgsConstructor
 public class KakaoAuthClient {
