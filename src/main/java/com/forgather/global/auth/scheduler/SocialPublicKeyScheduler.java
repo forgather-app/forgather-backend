c3fa1f8747b4fbf853ad3fffe13a0b29458b2957
kjyyjk <rlawnsdud920@gmail.com>
2026-07-05 13:39:48 +0900

    refactor: jwks 캐시 갱신 스케줄잡 로깅

--- src/main/java/com/forgather/global/auth/scheduler/SocialPublicKeyScheduler.java
+++ src/main/java/com/forgather/global/auth/scheduler/SocialPublicKeyScheduler.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Component;
 import com.forgather.global.auth.client.SocialAuthClient;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SocialPublicKeyScheduler {
@@ -15,6 +17,8 @@ public class SocialPublicKeyScheduler {
 
     @Scheduled(cron = "0 0 3 * * *") // 매일 새벽 3시 실행
     public void updateSocialPublicKeys() {
+        log.info("jwks 캐시 갱신 시작");
         socialAuthClient.updateAllKeys();
+        log.info("jwks 캐시 갱신 완료");
     }
 }
