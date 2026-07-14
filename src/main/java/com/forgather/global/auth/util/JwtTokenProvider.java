7b729172506142b6e7070cb7e6f7e03f6e1fa3ed
kjyyjk <rlawnsdud920@gmail.com>
2026-07-14 14:17:41 +0900

    refactor: 토큰 만료된 경우 남은 시간 0으로 반환

--- src/main/java/com/forgather/global/auth/util/JwtTokenProvider.java
+++ src/main/java/com/forgather/global/auth/util/JwtTokenProvider.java
@@ -12,6 +12,7 @@ import com.forgather.global.config.JwtProperties;
 import com.forgather.global.exception.JwtBaseException;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
@@ -94,8 +95,12 @@ public class JwtTokenProvider {
     }
 
     public long getRemainingExpirationSeconds(String token) {
-        long remainingMillis = getClaims(token).getExpiration().getTime() - System.currentTimeMillis();
-        return Math.max(remainingMillis / 1000, 0);
+        try {
+            long remainingMillis = getClaims(token).getExpiration().getTime() - System.currentTimeMillis();
+            return Math.max(remainingMillis / 1000, 0);
+        } catch (ExpiredJwtException e) {
+            return 0;
+        }
     }
 
     private Claims getClaims(String token) {
