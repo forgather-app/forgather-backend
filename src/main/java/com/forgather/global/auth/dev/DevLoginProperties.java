48aaa19699ee62fac3d5be9aaa54143c63a58bab
ykmxxi <ykmxxi97@gmail.com>
2026-07-18 15:18:09 +0900

    feat: 개발 임시 로그인 속성에 profile 명시 추가

--- src/main/java/com/forgather/global/auth/dev/DevLoginProperties.java
+++ src/main/java/com/forgather/global/auth/dev/DevLoginProperties.java
@@ -1,12 +1,14 @@
 package com.forgather.global.auth.dev;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@Profile({"local", "dev", "test"})
 @ConfigurationProperties(prefix = "auth.dev-login")
 public class DevLoginProperties {
 
